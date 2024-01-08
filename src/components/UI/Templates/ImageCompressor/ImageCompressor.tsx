2ccb91d554bd4889bc13dccf60b93ca278e8b1a2
LucaBn <luca.bonotti@virgilio.it>
2024-01-09 00:47:14 +0100

    feat(Image Compressor): :sparkles: complete image-compressor page with base ui

--- src/components/UI/Templates/ImageCompressor/ImageCompressor.tsx
+++ src/components/UI/Templates/ImageCompressor/ImageCompressor.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 // Components
+import { Col, Container, Row } from "react-bootstrap";
 import ImageCompressor from "@/components/UI/Organisms/ImageCompressor/ImageCompressor";
 
 const ImageCompressorTemplate: React.FC = () => {
   return (
-    <>
-      <h1>Image Compressor</h1>
-      <ImageCompressor />
-    </>
+    <Container className="py-5">
+      <Row>
+        <Col xs={12}>
+          <h1>Image Compressor</h1>
+        </Col>
+        <Col xs={12}>
+          <p>Select or drop an image, optimize its size and download it!</p>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col xs={12}>
+          <ImageCompressor />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
