5a9ce7112f125be6f0dc4f456ac285219d5d52f9
LucaBn <luca.bonotti@virgilio.it>
2024-01-23 22:26:36 +0100

    feat(Footer): :sparkles: add social share links

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,5 @@
 const APP_NAME: string = import.meta.env.VITE_APP_NAME;
 const AUTHOR_NAME: string = import.meta.env.VITE_AUTHOR_NAME;
+const WEBSITE_URL: string = import.meta.env.VITE_WEBSITE_URL;
 
-export { APP_NAME, AUTHOR_NAME };
+export { APP_NAME, AUTHOR_NAME, WEBSITE_URL };
