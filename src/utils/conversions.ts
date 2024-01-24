156b5206d8e91a56d1d6368d40bc5897d4d6c5b0
LucaBn <luca.bonotti@virgilio.it>
2024-01-24 23:49:53 +0100

    feat(ImageCompressor): :sparkles: improve compressed image description

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -4,7 +4,7 @@
  * @param {number} bytes - The number of bytes to be converted.
  * @returns {string} A string representing the human-readable format of the input bytes.
  */
-export const readableBytes = (bytes: number): string => {
+const readableBytes = (bytes: number): string => {
   // Calculate the appropriate unit (B, KB, MB, etc.) based on the logarithm to the base 1024.
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
 
@@ -14,3 +14,5 @@ export const readableBytes = (bytes: number): string => {
   // Calculate the converted value and append the corresponding unit.
   return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
 };
+
+export { readableBytes };
