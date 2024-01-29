186c5b178e2f0f84366f5398d416f50e2137ab2a
LucaBn <luca.bonotti@virgilio.it>
2024-01-29 22:44:57 +0100

    feat(Logo): :sparkles: update logo

--- src/components/UI/Molecules/Logo/Logo.tsx
+++ src/components/UI/Molecules/Logo/Logo.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 // Components
 import { Image } from "react-bootstrap";
+import Text from "@/components/UI/Atoms/Text/Text";
 
 // Constants
 import { APP_NAME } from "@/constants/app";
+import { CLASS_APP_NAME } from "@/constants/html-classes";
 
 interface ILogo {
   height?: number;
@@ -17,13 +19,23 @@ const Logo: React.FC<ILogo> = ({
   width = 100,
   title = APP_NAME, // Use APP_NAME as a fallback
 }) => {
+  const logoLabel = title.toUpperCase();
   return (
-    <Image
-      src={`/assets/img/logo.png`}
-      title={title}
-      height={height}
-      width={width}
-    />
+    <div className={`${CLASS_APP_NAME}__logo position-relative`}>
+      <Image
+        src={`/assets/img/logo.png`}
+        title={title}
+        height={height}
+        width={width}
+        className="me-1"
+      />
+      <Text
+        tag="span"
+        attributeList={{ className: `${CLASS_APP_NAME}__logo-label` }}
+      >
+        {logoLabel}
+      </Text>
+    </div>
   );
 };
 
