8012fcf2ce1efa2f98a84e28f1c13352e3b024dc
LucaBn <luca.bonotti@virgilio.it>
2024-01-31 22:03:42 +0100

    feat(BuyMeACoffee): :lipstick: change image

--- src/components/UI/Organisms/Footer/Footer.tsx
+++ src/components/UI/Organisms/Footer/Footer.tsx
@@ -123,7 +123,7 @@ const Footer: React.FC = () => {
                 <Image
                   src="/assets/img/buy-me-a-coffee.png"
                   width={200}
-                  className="d-block mx-auto"
+                  className="d-block mx-auto rounded"
                   alt={t("footer.buy-me-a-coffee")}
                 />
               </Link>
