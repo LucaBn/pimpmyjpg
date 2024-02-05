86510058ca4464f6298ef6c93e41d17825fd8b7f
LucaBn <luca.bonotti@virgilio.it>
2024-02-05 22:37:04 +0100

    refactor(Text): :recycle: break lines in long texts

--- src/components/UI/Organisms/ImageCompressor/ImageCompressor.tsx
+++ src/components/UI/Organisms/ImageCompressor/ImageCompressor.tsx
@@ -220,7 +220,7 @@ const ImageCompressor: React.FC = () => {
         <div className="d-flex flex-column align-items-center gap-3">
           {/* Drop picture(s) container */}
           <div className="image-compressor__upload-container bg-gradient rounded">
-            <label htmlFor="file-input" className="fs-5">
+            <label htmlFor="file-input" className="white-space-pre-line fs-5">
               {t("image-compressor.file-input-description")}
             </label>
             <input
