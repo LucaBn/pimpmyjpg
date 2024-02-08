e61014d6bf50acc00f53f5fc79244c1a8dc85062
LucaBn <luca.bonotti@virgilio.it>
2024-02-08 23:09:18 +0100

    feat(Routes): :truck: add watermark route

--- src/components/providers/ReactRouterProvider/ReactRouterProvider.tsx
+++ src/components/providers/ReactRouterProvider/ReactRouterProvider.tsx
@@ -5,6 +5,7 @@ import App from "@/components/App/App";
 import Error404 from "@/components/UI/Templates/Error404/Error404";
 import FeatureSelector from "@/components/UI/Templates/FeatureSelector/FeatureSelector";
 import ImageCompressor from "@/components/UI/Templates/ImageCompressor/ImageCompressor";
+import WatermarkHandler from "@/components/UI/Organisms/WatermarkHandler/WatermarkHandler";
 
 export const ReactRouterProvider = () => {
   return (
@@ -15,6 +16,7 @@ export const ReactRouterProvider = () => {
           <Route path=":language">
             <Route index element={<FeatureSelector />} />
             <Route path="image-compressor" element={<ImageCompressor />} />
+            <Route path="add-watermark" element={<WatermarkHandler />} />
             <Route path="*" element={<Error404 />} />
           </Route>
         </Route>
