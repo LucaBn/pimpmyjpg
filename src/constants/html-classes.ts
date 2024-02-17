7bbcb9e0569a8d397562bc7f6abb52bbbf4d56d6
LucaBn <luca.bonotti@virgilio.it>
2024-02-17 15:46:35 +0100

    feat(Env): :sparkles: update env variables

--- src/constants/html-classes.ts
+++ src/constants/html-classes.ts
@@ -1,8 +1,8 @@
 import { stringToValidClassName } from "@/utils/html-classes";
 
-import { APP_NAME } from "@/constants/app";
+import { APP_NAME_SHORT } from "@/constants/app";
 
-const CLASS_APP_NAME: string = stringToValidClassName(APP_NAME);
+const CLASS_APP_NAME: string = stringToValidClassName(APP_NAME_SHORT);
 const CUSTOM_PREFIX: string = `${CLASS_APP_NAME}-prefix`;
 const CUSTOM_SUFFIX: string = `${CLASS_APP_NAME}-suffix`;
 
